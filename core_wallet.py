97e5c9905e51718285b3b3baad4d075ad57bfef5
Albert Wang <albertofwb@users.noreply.github.com>
2024-08-21 18:20:49 +0800

    支持批量地址监控

--- core_wallet.py
+++ core_wallet.py
@@ -1,10 +1,7 @@
-import time
 from web3 import Web3
-from web3.exceptions import BlockNotFound
 from web3.middleware import geth_poa_middleware
-from abi import CORE_RPC_URL, MIN_GAS_PRICE
+from abi import CORE_RPC_URL
 from eth_utils import to_checksum_address
-from private import to_address
 from util import get_wallet_address_from_private_key
 
 
@@ -16,7 +13,7 @@ class CoreWallet:
             raise ConnectionError("无法连接到Core DAO网络")
         self.private_key = private_key
         self.from_address = to_checksum_address(get_wallet_address_from_private_key(private_key))
-        self.balance = float(self.get_core_balance())
+        # self.balance = float(self.get_core_balance())
 
     def is_valid_address(self, address):
         return self.w3.is_address(address)
@@ -55,44 +52,3 @@ class CoreWallet:
         print(f"transfer {self.from_address} => {to_address} {amount} CORE")
         return self.w3.to_hex(tx_hash)
 
-    def monitor_transactions(self, callback):
-        """
-                Monitor real-time transactions for a specific address.
-
-                :param address: The address to monitor
-                :param callback: A function to call when a new transaction is detected
-                """
-        address = self.from_address
-        if self.balance > MIN_GAS_PRICE:
-            self.transfer(to_address, self.balance - MIN_GAS_PRICE, False)
-        print(f"开始监控地址 {address} 的交易...")
-        print(f"余额: {self.get_core_balance(address):,.3f} CORE")
-        last_block = self.w3.eth.get_block_number()
-
-        try:
-            while True:
-                current_block = self.w3.eth.get_block_number()
-
-                for block_number in range(last_block + 1, current_block + 1):
-                    try:
-                        block = self.w3.eth.get_block(block_number, full_transactions=True)
-                    except BlockNotFound:
-                        time.sleep(1)
-                        continue
-                    for tx in block.transactions:
-                        if tx['to'] == address:
-                            callback(self, tx)
-                last_block = current_block
-                time.sleep(1)  # Wait for 1 second before checking again
-        except KeyboardInterrupt:
-            print("监控已停止")
-
-
-def transaction_callback(wallet: CoreWallet, tx):
-    w3 = wallet.w3
-    print(f"monitor {tx['from']} => {tx['to']} {w3.from_wei(tx['value'], 'ether')} CORE")
-    amount = float(w3.from_wei(tx['value'], 'ether')) - MIN_GAS_PRICE  # 扣除手续费
-    try:
-        wallet.transfer(to_address, amount, False)
-    except ValueError as e:
-        print(e)
--- monitor.py
+++ monitor.py
@@ -1,7 +1,64 @@
-from core_wallet import CoreWallet, transaction_callback
-from private import private_key, to_address
+import asyncio
+from web3 import AsyncWeb3
+from web3.exceptions import BlockNotFound
+from web3.middleware import async_geth_poa_middleware
+from abi import MIN_GAS_PRICE, CORE_RPC_URL
+from core_wallet import CoreWallet
+from private import to_address, monitor_wallets
+from util import get_wallet_address_from_private_key
+
+
+class Monitor:
+    def __init__(self, private_keys: [str]):
+        self.private_keys = private_keys
+        self.w3 = AsyncWeb3(AsyncWeb3.AsyncHTTPProvider(CORE_RPC_URL))
+        self.w3.middleware_onion.inject(async_geth_poa_middleware, layer=0)
+        self.private_map_address = self.construct_addresses()
+
+    def construct_addresses(self):
+        address_map = {}
+        for i in self.private_keys:
+            from_address = get_wallet_address_from_private_key(i)
+            address_map[from_address] = i
+        return address_map
+
+    async def transfer(self, private_key, to_address, amount):
+        wallet = CoreWallet(private_key)
+        loop = asyncio.get_running_loop()
+        await loop.run_in_executor(None, wallet.transfer, to_address, amount, False)
+
+    async def monitor_transactions(self):
+        monitor_addresses = self.private_map_address.keys()
+        print(f"开始监控地址 {monitor_addresses} 的交易...")
+        last_block = await self.w3.eth.get_block_number()
+        try:
+            while True:
+                current_block = await self.w3.eth.get_block_number()
+
+                for block_number in range(last_block + 1, current_block + 1):
+                    try:
+                        block = await self.w3.eth.get_block(block_number, full_transactions=True)
+                    except BlockNotFound:
+                        await asyncio.sleep(1)
+                        continue
+                    for tx in block.transactions:
+                        if tx['to'] in monitor_addresses:
+                            private_key = self.private_map_address[tx['to']]
+                            amount = float(self.w3.from_wei(tx['value'], 'ether')) - MIN_GAS_PRICE
+                            try:
+                                await self.transfer(private_key, to_address, amount)
+                            except Exception as ex:
+                                print(f"转账失败: {tx['to']} => {to_address} {amount} CORE", ex)
+                last_block = current_block
+                await asyncio.sleep(1)  # Wait for 1 second before checking again
+        except KeyboardInterrupt:
+            print("监控已停止")
+
+
+async def main():
+    monitor = Monitor(monitor_wallets)
+    await monitor.monitor_transactions()
+
 
 if __name__ == '__main__':
-    wallet = CoreWallet(private_key)
-    wallet.get_core_balance(to_address)
-    wallet.monitor_transactions(transaction_callback)
+    asyncio.run(main())
